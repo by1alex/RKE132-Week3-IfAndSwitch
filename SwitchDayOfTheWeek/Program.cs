e69d466f44d0ca12e6c7e684f0ea2917fe01b8bb
Aleksandr Bychkov <Aleksandr.bychkov@tktk.ee>
2023-09-20 20:42:15 +0300

    first commit

---
+++ SwitchDayOfTheWeek/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek); //0-6
+
+switch(weekDay) //vaartus, mida kontrollitakse
+{
+    case 0: // case 0: == if(weekday ==0)
+        Console.WriteLine("It's Sunday!");
+        break;
+    case 1:
+        Console.WriteLine("It's Monday!");
+        break;
+    case 2:
+        Console.WriteLine("It's Tueday!");
+        break;
+    case 3:
+        Console.WriteLine("It's Wednesday!");
+        break;
+    case 4:
+        Console.WriteLine("It's Thursday!");
+        break;
+    case 5:
+        Console.WriteLine("It's Friday!");
+        break;
+    case 6:
+        Console.WriteLine("It's Saturday!");
+        break;
+}
+
+Console.WriteLine("Have a nice day!");
